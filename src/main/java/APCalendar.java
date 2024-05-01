5a1a5345c6d079de60e1b5b67da15f0e05b1a147
JustinWChen <143028009+JustinWChen@users.noreply.github.com>
2024-04-30 19:00:11 -0700

    Update APCalendar.java

--- src/main/java/APCalendar.java
+++ src/main/java/APCalendar.java
@@ -57,5 +57,10 @@ int count = 0;
   public static int dayOfWeek(int month, int day, int year)
   {
     /* to be implemented in part (b) */
+    int startDay = firstDayOfYear(year);
+    int nthDay = dayOfYear(month, day, year);
+    int returnDay = (startDay + nthDay -1)%7;
+    return returnDay;
+    
   }
 }
